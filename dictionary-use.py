7a599b0ae89f4546875758871ad1894d24bbe18f
Mehmood <halcyoona@gmail.com>
2018-08-02 12:26:50 +0500

    g

---
+++ dictionary-use.py
@@ -0,0 +1,17 @@
+
+
+fruit = {
+    'apple': 6,
+    'banana':12,
+    'orange':8,
+    'grapes':4
+}
+quantity ={
+    'apple':32,
+    'banana':24,
+    'orange':100,
+    'grapes':4
+}
+for k in fruit:  
+    print(fruit[k],k,quantity[k] , 'per kg')
+    break
